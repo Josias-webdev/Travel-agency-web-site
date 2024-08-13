30d765aba6fa6426b21d479eef795f58e6000fde
Josias-webdev <joejosiasb@gmail.com>
2024-08-13 23:15:00 +0100

    some changes for deployement on heroku

--- AgenceDeVoyage/settings.py
+++ AgenceDeVoyage/settings.py
@@ -91,13 +91,22 @@ WSGI_APPLICATION = 'AgenceDeVoyage.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'oceanreservation_db',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
